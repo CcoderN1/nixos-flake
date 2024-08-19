0f4967e186ad68bdf5497d4af0db271275b1e4a9
unixlike <billcipherjava@gmail.com>
2024-08-19 04:51:12 +0300

    Fixed chrome

--- overlays/chrome.nix
+++ overlays/chrome.nix
@@ -5,6 +5,6 @@
   commandLineArgs = [
     "--enable-features=UseOzonePlatform"
     "--ozone-platform=wayland"
-    "--proxy-server="http://127.0.0.1:8080""
+    "--proxy-server='http://127.0.0.1:8080'"
   ];
 })
